fb3cb7d63dcbbb8f3d98d25e1329c101634468c6
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-03-16 20:26:08 +0530

    added

---
+++ date.php
@@ -0,0 +1,13 @@
+<?php 
+    // echo date('d-m-y');
+    // echo date('h i s a');
+    // $d=strtotime("10:30pm April 15 2014");
+    // echo "Created date is " . date("Y-m-d h:i:sa", $d);
+    // $startdate = strtotime("Saturday");
+    // $enddate = strtotime("+6 weeks", $startdate);
+
+    // while ($startdate < $enddate) {
+    //         echo date("M d", $startdate) . "<br>";
+    //         $startdate = strtotime("+1 week", $startdate);
+    // }   
+?>
---
+++ file.php
@@ -0,0 +1,13 @@
+<?php 
+    // $file = fopen("webdictionary.txt", "r") or die("File not opened");
+    // echo fread($file, filesize("webdictionary.txt"));
+    // $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+    // while(!feof($myfile)) {
+    // echo fgets($myfile) . "<br>";
+    // }
+    // fclose($myfile);
+    // $myfile = fopen("testfile.txt", "w")
+    // or die("Unable to open file!");
+    // fwrite($myfile, "Hello world!");
+    // fclose($myfile);
+?>
