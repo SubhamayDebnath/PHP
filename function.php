a15f05b44cbfc47b1e8598970272586a846b3841
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-03-16 17:23:14 +0530

    added

---
+++ function.php
@@ -0,0 +1,18 @@
+<?php 
+    // function getName($name){
+    //     return $name;
+    // }
+    // function func($name){
+    //     return getName($name);
+    // }
+    // echo func("hello");
+    // function num1($num){
+    //     return $num * 2;
+    // }
+    // $num = "num1";
+    // function num2($num){
+    //     return $num(5);
+    // }
+    // echo num2($num);
+
+?>
