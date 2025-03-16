dda24126d580a8d0e99108c21734672026c0706d
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-03-16 21:48:51 +0530

    added

---
+++ cookie_and_session.php
@@ -0,0 +1,13 @@
+<?php
+    // echo "Cookie";
+    // setcookie("user", "John Doe", time() + (86400 * 30), "/", "localhost", true, true);
+    // print_r($_COOKIE);
+    
+    // session_start();
+    // $_SESSION['user'] = "John Do3";
+    // print_r($_SESSION);
+    // echo "<br/>";
+    // session_unset();
+    // session_destroy();
+    // print_r($_SESSION);
+?>
