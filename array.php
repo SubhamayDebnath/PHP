4d18cbc262f89434cfb5db1486dd2e5fa758d6b5
SubhamayDebnath <subhamaydebnath6@gmail.com>
2025-03-16 17:57:09 +0530

    `Added array manipulation examples to array.php`

--- array.php
+++ array.php
@@ -1,3 +1,19 @@
 <?php 
-    echo "array";
+    // $arr = array('hello', 'world','bro');
+    // foreach($arr as $value){
+    //     echo $value."<br/>";
+    // }
+    // echo "Change Array <br/>";
+    // $arr[2]='Sir';
+    // foreach($arr as $value){
+    //     echo $value."<br/>";
+    // }
+
+    // $car = array("brand"=>"Ford", "model"=>"Mustang", "year"=>1964);
+    // echo $car['year'];
+
+    $arr = array(1,2,3,4,5,6);
+    // array_push($arr, 7);
+    // array_pop($arr);
+    // print_r ($arr);
 ?>
